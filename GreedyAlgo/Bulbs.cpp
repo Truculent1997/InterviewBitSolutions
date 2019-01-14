f6fa06ba58da697d074d57404c559549dca3ac55
Truculent1997 <46653003+Truculent1997@users.noreply.github.com>
2019-01-14 20:19:53 +0530

    Create Bulbs.cpp

---
+++ GreedyAlgo/Bulbs.cpp
@@ -0,0 +1,11 @@
+int Solution::bulbs(vector<int> &A) {
+    int c=0;
+    int n=A.size();
+    int i=0;
+    while(i<n){
+        if((c+A[i])){}
+        else c++;
+        i++;
+    }
+    return c;
+}
